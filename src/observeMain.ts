ffec53686c5a60d13c2345f13d8a374f1a7e0fa1
RebikHub <rebikovlk@mail.ru>
2025-07-22 17:52:35 +0200

    test lib & add requests

--- src/observeMain.ts
+++ src/observeMain.ts
@@ -1,72 +1,113 @@
-import './style.css'
-import javascriptLogo from '../public/javascript.svg'
-import viteLogo from '../public/vite.svg'
-import { createComponent, createState, observe } from '@lib/index'
+import "./style.css";
+import javascriptLogo from "../public/javascript.svg";
+import viteLogo from "../public/vite.svg";
+import { createComponent, createState, observe } from "@lib/index";
 
 interface initialState {
-  count: number
+  count: number;
 }
 
 const store = createState<initialState>({
-  count: 0
-})
+  count: 0,
+});
 
-const LinkImage = ({ href, className, alt, srcLogic }: any): HTMLElement => createComponent({
-  tag: 'a',
-  href,
-  children: [observe({
+const LinkImage = ({ href, className, alt, srcLogic }: any): HTMLElement =>
+  createComponent({
+    tag: "a",
+    href,
+    children: [
+      observe({
+        store,
+        props: {
+          tag: "img",
+          class: className,
+          alt,
+          src: srcLogic(store.state.count),
+        },
+        render: (state: initialState): { src: string } => ({
+          src: srcLogic(state.count),
+        }),
+      }),
+    ],
+  });
+
+const CounterButton = (): HTMLElement =>
+  observe({
     store,
     props: {
-      tag: 'img',
-      class: className,
-      alt,
-      src: srcLogic(store.state.count)
+      tag: "button",
+      id: "counter",
+      type: "button",
+      content: `count is ${store.state.count}`,
+      events: {
+        click: () => store.set({ count: store.state.count + 1 }),
+      },
     },
-    render: (state: initialState): { src: string } => ({ src: srcLogic(state.count) })
-  })]
-})
+    render: (state: initialState): { content: string } => ({
+      content: `count is ${state.count}`,
+    }),
+  });
 
-const CounterButton = (): HTMLElement => observe({
-  store,
-  props: {
-    tag: 'button',
-    id: 'counter',
-    type: 'button',
-    content: `count is ${store.state.count}`,
-    events: {
-      click: () => store.set({ count: store.state.count + 1 })
+const createUseState = <T>(
+  state: T
+): [T, (callback: T | ((prevState: T) => T)) => void] => {
+  let curState = state;
+
+  const callback = (prev: T | ((prevState: T) => T)): void => {
+    if (typeof prev === "function") {
+      curState = (prev as (prevState: T) => T)(curState);
+    } else {
+      curState = prev;
     }
-  },
-  render: (state: initialState): { content: string } => ({ content: `count is ${state.count}` })
-})
+  };
 
-export const App = (): HTMLElement =>
-  createComponent({
+  return [curState, callback];
+};
+
+export const App = (): HTMLElement => {
+  const [click, setClick] = createUseState<number>(0);
+  console.log("state value: ", click);
+  // console.log('state setter: ', setClick)
+
+  return createComponent({
     children: [
       LinkImage({
-        href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-        className: 'logo',
-        alt: 'Vite logo',
-        srcLogic: (count: number) => (count % 2 === 0 ? javascriptLogo : viteLogo)
+        href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        className: "logo",
+        alt: "Vite logo",
+        srcLogic: (count: number) =>
+          count % 2 === 0 ? javascriptLogo : viteLogo,
       }),
       LinkImage({
-        href: 'https://vitejs.dev',
-        className: 'logo vanilla',
-        alt: 'JavaScript logo',
-        srcLogic: (count: number) => (count % 2 !== 0 ? javascriptLogo : viteLogo)
+        href: "https://vitejs.dev",
+        className: "logo vanilla",
+        alt: "JavaScript logo",
+        srcLogic: (count: number) =>
+          count % 2 !== 0 ? javascriptLogo : viteLogo,
       }),
       createComponent({
-        tag: 'h1',
-        content: 'Hello Vite!'
+        tag: "h5",
+        content: "Hello Vite!",
       }),
       createComponent({
-        class: 'card',
-        children: [CounterButton]
+        class: "card",
+        children: [CounterButton],
       }),
       createComponent({
-        tag: 'p',
-        class: 'read-the-docs',
-        content: 'Click on the Vite logo to learn more'
-      })
-    ]
-  })
+        tag: "p",
+        class: "read-the-docs",
+        content: `Click on the Vite logo to learn more & ${click}`,
+        events: {
+          click: () => {
+            console.log("click", click);
+
+            setClick((prev: number): number => {
+              console.log("setClick event: ", prev);
+              return prev + 1;
+            });
+          },
+        },
+      }),
+    ],
+  });
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,14 @@
-import { createRouter } from '../lib'
-import { App } from './observeMain'
-import { About } from './pages/about'
-import { Home } from './pages/home'
-import { Main } from './pages/main/view/main'
+import { createRouter } from "../lib";
+import { App } from "./observeMain";
+import { About } from "./pages/about";
+import { Home } from "./pages/home";
+import { Main } from "./pages/main/view/main";
 
-export const { add, layout, navigate, start } = createRouter()
+export const { add, layout, navigate, start } = createRouter();
 
-add('/switch', App)
-
-add('/about', About)
-
-add('/main', Main)
-
-add('/', Home)
+[
+  { route: "/switch", component: App },
+  { route: "/about", component: About },
+  { route: "/main", component: Main },
+  { route: "/", component: Home },
+].forEach(({ route, component }) => add(route, component));
